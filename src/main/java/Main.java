1045424f59c01753ad62bab07d9fd272cb72e31f
Lucine Shahgholian <luc.golian@gmail.com>
2019-01-24 11:29:49 +0100

    Printed treasure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,23 +25,43 @@ public class Main {
         terminal.setCursorVisible(false);
 
         Player tom = new Player(0, 2);
-        Treasure treasure1 = new Treasure();
+//        Treasure treasure1 = new Treasure();
+        Obstacle obstacle = new Obstacle(25, 16);
 
         boolean playerIsAlive=true;
 
+        obstacle.printBlock(3, 3, terminal,obstacle);
         while (playerIsAlive) {
 
-            drawBoard(terminal, tom, treasure1);
+            drawBoard(terminal, tom, obstacle);
+
         }
     }
 
-    public static void drawBoard(Terminal terminal, Player tom, Treasure treasure1) throws IOException {
+    public static void drawBoard(Terminal terminal, Player tom, Obstacle obstacle) throws IOException {
         terminal.setCursorPosition(tom.getX(), tom.getY());         //printing tom
         terminal.putCharacter(tom.getSymbol());
 
-        terminal.setCursorPosition(treasure1.randomX(), treasure1.randomY());         // printing treasure
-        terminal.putCharacter(treasure1.getSymbol());
+//        terminal.setCursorPosition(treasure1.randomX(), treasure1.randomY());         // printing treasure
+//        terminal.putCharacter(treasure1.getSymbol());
+//        terminal.flush();
+//
+//        terminal.setCursorPosition(25, 16);         //i=0
+//        terminal.putCharacter(obstacle.getSymbol());
+//        terminal.setCursorPosition(26, 16);         //i=1
+//        terminal.putCharacter(obstacle.getSymbol());
+//        terminal.setCursorPosition(25, 17);         //i=2
+//        terminal.putCharacter(obstacle.getSymbol());
+//        terminal.setCursorPosition(26, 17);         //i=3
+//        terminal.putCharacter(obstacle.getSymbol());
+//
+
         terminal.flush();
+
+         //   terminal.setCursorPosition();         //printing obstacle
+//            terminal.putCharacter(obstacle.getSymbol());
+//           obstacle.printBlock(25, 16);
+
+        }
     }
 
-}
