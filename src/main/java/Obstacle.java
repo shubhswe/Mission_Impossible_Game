f1da9427aaafef7e98e27d61257a2cd573e017d5
Jungha Lee <junghalee001@gmail.com>
2019-01-24 15:25:20 +0100

    Merge branch 'master' of C:\Users\Jungha Lee\Dropbox\Java\0037.W03-MiniProject1\Mission_Impossible_Game with conflicts.

--- src/main/java/Obstacle.java
+++ src/main/java/Obstacle.java
@@ -1,20 +1,8 @@
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
-import java.io.IOException;
-import java.io.InvalidObjectException;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Obstacle {
 
     private int x;
     private int y;
     private char symbol = '\u2588';
-
-
-    public char getSymbol() {
-        return symbol;
-    }
 
     public Obstacle(int x, int y) {
         this.x = x;
@@ -37,23 +25,8 @@ public class Obstacle {
         this.y = y;
     }
 
-    public void printBlock(int sizeX, int sizeY, Terminal terminal, Obstacle obstacle) throws IOException {
 
 
-        int posX = ThreadLocalRandom.current().nextInt(5, 75);
-        int posY = ThreadLocalRandom.current().nextInt(5, 19);
-
-        for (int i = 0; i < sizeY; i++) {
-            for (int j = 0; j < sizeX; j++) {
-
-                terminal.setCursorPosition(posX, posY);
-                terminal.putCharacter(obstacle.getSymbol());
-                posX++;
-            }
-            posX = posX - sizeX;
-            posY++;
 
 
-        }
-    }
 }
